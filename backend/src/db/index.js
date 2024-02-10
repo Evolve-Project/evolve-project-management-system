8d44f3be1c725eb33d647d5d5aaa84ef33afad59
Purnima3 <purnimakabadwal3@gmail.com>
2024-02-11 04:42:39 +0530

    removed .connect() from index.js of db

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -8,14 +8,7 @@ const pool =  new Pool({
     port: 5432,
 });
 
-pool.connect()
-  .then(() => {
-    console.log('Database connected...');
-    // Execute queries here
-  })
-  .catch(err => {
-    console.error('Error connecting:', err);
-  });
+
 
 module.exports = {
     query: (text, params) => pool.query(text,params),
