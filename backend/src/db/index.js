c591759f03870ca09cbf428fd948dcac7d1b865d
Harsh Verma <harshvse@gmail.com>
2024-02-11 14:58:41 +0530

    changed db credentials

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,15 +1,15 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
-const pool =  new Pool({
+const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'TasksMilestones',
-    password: 'Mylove123',
+    database: 'evolve',
+    password: 'postgres',
     port: 5432,
 });
 
 
 
 module.exports = {
-    query: (text, params) => pool.query(text,params),
+    query: (text, params) => pool.query(text, params),
 }
