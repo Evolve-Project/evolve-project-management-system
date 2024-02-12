d70c07a5fb9a00f5a46629c207340c82663d93ac
VIJAY R <125776700+Vijaydrravi@users.noreply.github.com>
2024-02-12 11:44:55 +0530

    Create Chat.jsx

---
+++ Queries-Asking/Components/Chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ChatApp from './ChatApp'
+import './Chat.css'
+const Chat = () => {
+  return (
+    <div className='parent'>
+    <div className="container">
+        <div className="contain">
+           <ChatApp />
+        </div>
+       </div>
+       </div>
+  )
+}
+
+export default Chat
