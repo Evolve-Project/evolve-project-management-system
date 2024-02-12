8318eecc01bae64c4fdc8485db31a3dc33da83d4
ashugyl75 <ashugyl75@gmail.com>
2024-02-12 13:01:46 +0530

    deleted redundant files

--- backend/database.sql
+++
@@ -1,8 +0,0 @@
--- Users Table
-create table users(
-    user_id serial primary key, 
-    email varchar(255) unique not null, 
-    role varchar(255) not null,
-    password varchar(255) not null,
-    created_at date default current_date
-);
