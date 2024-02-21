b6d891054752e82099be764c023b4b70daea2c97
Purnima3 <purnimakabadwal3@gmail.com>
2024-02-22 00:26:15 +0530

    debug feedback link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,9 +56,8 @@ const App = () => {
           {role === "Mentor" && (
             <>
               <Route path="/dashboard" element={<DashboardMentor />} />
-              <Route path="/feedback" element={<Feedback />} />
+              <Route path="/feedback" element={<MentorFeedback />} />
               <Route path="/query" element={<Query />} />
-              <Route path="/attendance" element={<Query />} />
               <Route path="/milestones" element={<MentorMilestone/>} />
             </>
           )}
