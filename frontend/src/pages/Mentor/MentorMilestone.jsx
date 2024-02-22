7d9ed747ae5ad4cbf93afa5995e8fb35da4c032b
Purnima3 <purnimakabadwal3@gmail.com>
2024-02-22 10:32:43 +0530

    Added theme and  Background to milestone pages.

--- frontend/src/pages/Mentor/MentorMilestone.jsx
+++ frontend/src/pages/Mentor/MentorMilestone.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { loadMilestones } from '../../api/milestone';
+import {Box , Heading}  from "@chakra-ui/react"
+
+import MilestoneTable from './MilestoneTable';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from "../../components/theme/theme.jsx"
 
 
 function MentorMilestone() {
@@ -21,7 +26,12 @@ fetchMilestones();
   return (
     <div>
       <h1>Mentor Milestone</h1>
-      
+      <ChakraProvider theme={theme}>
+      <Box maxW = {1000} mx="auto" px={6} frontSize="sm"> 
+      <Heading ab={10}>TenStack Table</Heading>
+      <MilestoneTable/>
+      </Box>
+      </ChakraProvider>
     </div>
   )
 }
