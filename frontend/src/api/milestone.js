5cc338d981a57a2c13e98b34f9dfa8041f0d53ae
Purnima3 <purnimakabadwal3@gmail.com>
2024-02-22 08:16:04 +0530

    /get-milestones

--- frontend/src/api/milestone.js
+++ frontend/src/api/milestone.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 
 export async function loadMilestones() {
-    return await axios.get('http://localhost:8000/api//get-milestones')
+    return await axios.get('http://localhost:8000/api/get-milestones')
 }
--- frontend/src/pages/Mentor/MentorMilestone.jsx
+++ frontend/src/pages/Mentor/MentorMilestone.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { loadMilestones } from '../../api/milestone';
+
 
 function MentorMilestone() {
+    
+
+async function fetchMilestones() {
+    try {
+        const response = await loadMilestones();
+        // Handle the response data here
+        console.log(response.data);
+    } catch (error) {
+        // Handle errors here
+        console.error('Error loading milestones:', error);
+    }
+}
+
+fetchMilestones();
+    
   return (
     <div>
       <h1>Mentor Milestone</h1>
+      
     </div>
   )
 }
