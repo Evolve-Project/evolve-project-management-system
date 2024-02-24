4bfc45c57ca2ab421ac49ee5976f8e759e999baa
Purnima3 <purnimakabadwal3@gmail.com>
2024-02-24 21:56:26 +0530

    set table

--- frontend/src/pages/Mentor/milestones/MentorMilestones.jsx
+++ frontend/src/pages/Mentor/milestones/MentorMilestones.jsx
@@ -37,10 +37,10 @@ function MentorMilestones() {
     <div>
       <Button></Button>
         
-        <h1>Mentee Milestone</h1>
+        
       <ChakraProvider theme={theme}>
       <Box maxW = {1000} mx="auto" px={6} frontSize="sm"> 
-      <Heading ab={10}>Milestones</Heading>
+      <Heading ab={8}>Milestones</Heading>
       <table className="table-auto border-collapse w-full">
             <thead>
               <tr className="bg-gray-200 text-black-600 uppercase text-sm leading-normal">
@@ -57,26 +57,20 @@ function MentorMilestones() {
             <tbody className="text-black-600 text-bg font-dark">
               {milestoneDesc.map((milestone, index) => (
                 <tr key={index} className="border-b border-gray-200 hover:bg-gray-100">
-                  <td className="py-3 px-5 text-left whitespace-nowrap">{milestone.name}</td>
-                  <td className="py-3 px-5 text-left">{milestone.description}</td>
-                  <td className="py-3 px-5 text-left">{formatDate(milestone.start_date)}</td>
-                  <td className="py-3 px-5 text-left">{formatDate(milestone.end_date)}</td>
-                  <td className="py-3 px-6 text-left relative">
+                  <td className="py-2 px-4 text-left whitespace-nowrap">{milestone.name}</td>
+                  <td className="py-2 px-4 text-left">{milestone.description}</td>
+                  <td className="py-2 px-4 text-left">{formatDate(milestone.start_date)}</td>
+                  <td className="py-2 px-4 text-left">{formatDate(milestone.end_date)}</td>
+                  <td className="py-2 px-4 text-left relative">
                     <button
                       className="bg-blue-500 text-white py-1 px-4 rounded-full"
                       onClick={() => setShowDropdown(!showDropdown)} // Toggle dropdown on click
                     >
                       On Progress
                     </button>
-                    {/* Show dropdown only if showDropdown is true */}
-                    {showDropdown && (
-                      <div className="absolute mt-1 w-32 bg-white rounded-md shadow-lg">
-                        <button className="block py-2 px-4 text-gray-800 hover:bg-gray-100 w-full text-left">On Progress</button>
-                        <button className="block py-2 px-4 text-gray-800 hover:bg-gray-100 w-full text-left">Done</button>
-                      </div>
-                    )}
+                   
                   </td>
-                  <td className="py-3 px-6 text-left">
+                  <td className="py-2 px-4 text-left">
                     <button className="bg-gray-200 text-gray-800 py-1 px-4 rounded-full">View Details</button>
                   </td>
                 </tr>
